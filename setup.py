04de1e967f679c586cb33bb7fb0597d1ca233e24
Patrick Michaud <pmichaud@uw.edu>
2014-04-08 21:41:14 -0700

    Adding setup/submodule

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import subprocess
+import sys
+import os
+
+path = os.path.dirname(os.path.realpath(__file__))
+
+submodule_path = os.path.join(path, "spacescout_web")
+
+subprocess.call(["git", "submodule", "init"], cwd=path)
+subprocess.call(["git", "submodule", "update"], cwd=path)
+
+
+setup(name='SpaceScout-Web-Wrapper',
+      version='1.0',
+)
+
+subprocess.call(["pip", "install", "-r", "requirements.txt"], cwd=submodule_path)
